f506e5acd89b1c1abe31db883b37cae165c2d809
Sravan Karthik <77857519+sravankarthik@users.noreply.github.com>
2023-01-13 13:00:26 +0530

    second_commit

--- controllers/voter.js
+++ controllers/voter.js
@@ -1,18 +1,76 @@
+const { getVoterById_service, getVoters_service, updateVoter_service, deleteVoter_service } = require("../services/voter")
+
 //param
-const getVoterById = () => {
-
+const getVoterById = (req, res) => {
+    const id = req.params.id;
+    getVoterById_service(id, (err, results) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if (!results) {
+            return res.json({
+                success: 0,
+                message: "Record not Found"
+            });
+        }
+        results.password = undefined;
+        return res.json({
+            success: 1,
+            data: results
+        });
+    });
 }
 
-const getVoters = () => {
-
+const getVoters = (req, res) => {
+    getVoters_service((err, results) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        return res.json({
+            success: 1,
+            data: results
+        });
+    });
 }
 
-const updateVoter = () => {
-
+const updateVoter = (req, res) => {
+    const body = req.body;
+    const saltRounds = 10;
+    bcrypt.hash(body.password, saltRounds, function (err, hash) {
+        body.password = hash
+    });
+    updateVoter_service(body, (err, results) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        return res.json({
+            success: 1,
+            message: "updated successfully"
+        });
+    });
 }
 
-const deleteVoter = () => {
-
+const deleteVoter = (req, res) => {
+    const data = req.body;
+    deleteVoter_service(data, (err, results) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if (!results) {
+            return res.json({
+                success: 0,
+                message: "Record Not Found"
+            });
+        }
+        return res.json({
+            success: 1,
+            message: "user deleted successfully"
+        });
+    });
 }
 
 module.exports = { getVoterById, getVoters, updateVoter, deleteVoter };
